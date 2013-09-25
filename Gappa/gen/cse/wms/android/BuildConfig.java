1962bfc4d064d43b0bf4d2a5b0bc3b382a6d9cfb
sorez <cdwsorez@gmail.com>
2013-09-25 11:08:36 +0900

    Barcode Reader for Android

---
+++ Gappa/gen/cse/wms/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cse.wms.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Gappa/src/cse/wms/android/db/BasicNameValuePair.java
@@ -0,0 +1,23 @@
+package cse.wms.android.db;
+
+import org.apache.http.NameValuePair;
+
+public class BasicNameValuePair implements NameValuePair {
+	public BasicNameValuePair() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
